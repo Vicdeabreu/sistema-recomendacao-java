9115f6dd1b7160ca36a3e0d2f6967a6608a262d1
Víctor De Abreu <55564718+Vicdeabreu@users.noreply.github.com>
2020-05-31 22:08:55 -0300

    Exercicio de recomendacoes feito em Java

---
+++ src/main/java/digitalinovatioone/RecomendaFilmes.java
@@ -0,0 +1,27 @@
+package digitalinovatioone;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+
+import org.apache.mahout.cf.taste.model.DataModel;
+
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+
+import org.apache.mahout.cf.taste.recommender.Recommender;
+
+import java.io.IOException;
+
+import java.util.List;
+
+public class RecomendaFilmes{
+  
+  public static void main(String[] args) throws IOException,TasteException{
+    DataModel filmes = new Recomendador().getModeloDeFilmes();
+    Recommender recommender = new RecomendadorBuilder().buildRecommender(filmes);
+
+    List<RecommendedItem> recommendations = recommender.recommend(4, 3);
+    for (RecommendedItem recommendation :recommendations) { 
+      System.out.println("voce pode gostar deste filme");
+      System.out.println(recommendation);
+    }
+  }
+}
